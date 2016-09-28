4023e6da17a3daae4cbf4b4cadb3772ff82c1b49
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-28 11:06:33 +0100

    completed stage 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,16 @@
 class DockingStation
+
+		def initialize
+			@bike
+		end
+
+		attr_reader :bike
+
     def release_bike
     Bike.new
+    end
+
+    def dock(bike)
+    	@bike = bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,4 +9,8 @@ describe DockingStation do
         bike = subject.release_bike
         expect(bike).to be_working
     end
+    
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it { is_expected.to respond_to(:bike) }
+
 end
