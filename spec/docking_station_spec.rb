13b7b3cdf05f12acb7da89a91a9937c198cb521b
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-28 11:44:21 +0100

    completed step12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,6 +6,7 @@ describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
     it 'releases working bike' do
+    		subject.dock(Bike.new)
         bike = subject.release_bike
         expect(bike).to be_working
     end
@@ -13,4 +14,7 @@ describe DockingStation do
     it { is_expected.to respond_to(:dock).with(1).argument }
     it { is_expected.to respond_to(:bike) }
 
+    it 'raises an error when there are no bikes to release' do
+    	expect{ subject.release_bike }.to raise_error "No bikes available"
+    end
 end
