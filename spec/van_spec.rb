18841c204269d440228686cf044e07264cc7ceb9
antonyCastineiras <antonycastineiras80@gmail.com>
2016-09-29 16:51:11 +0100

    wrote simple tests for van class

---
+++ spec/van_spec.rb
@@ -0,0 +1,7 @@
+describe Van do 
+
+	it { is_expected.to respond_to :collect_broken_bikes }
+	it { is_expected.to respond_to :deliver_broken_bikes }
+	it { is_expected.to respond_to :deliver_working_bikes }
+	it { is_expected.to respond_to :collect_working_bikes }
+end
